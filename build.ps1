5cd79cb4b0add330a79869bc30eca18c9e7676b3
Maksim Makushchenko <maksalmak@gmail.com>
2021-01-19 14:10:10 +0300

    Update pathes in build script

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 npm run build
-ls .\deploy\ITLab-Front\js | %{
+ls ./deploy/ITLab-Front/js | %{
     $fname=$_.ToString();
     $start = $fname.IndexOf("app");
     $end = $fname.IndexOf("js") + 2;
     if ($start -gt -1 -and $end -gt -1) {
         $fname.Substring($start, $end);
     }
-} | Out-File ".\deploy\ITLab-Front\app.txt"
+} | Out-File "./deploy/ITLab-Front/app.txt"
