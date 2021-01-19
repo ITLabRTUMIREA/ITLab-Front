e5b81e4347629d1fba650a6728081fac8c01d465
IvLaptev <ivlaptev13@ya.ru>
2021-01-19 21:56:39 +0300

    Deleted summary tab

--- src/components/modals/SummaryModal.vue
+++ src/components/modals/SummaryModal.vue
@@ -5,45 +5,28 @@
       <template slot="modal-title">
         Итоговый отчёт
       </template>
-
-      <b-tabs>
-        <b-tab title="По типу" active @click="setMode(true)">
+      
+      <b-form-group label="Выберите период:">
+        <b-row>
+          <b-col cols="12" sm="6" md="12" lg="6" class="mt-3">
+            <date-picker class="w-100" v-model="startDate" lang="ru" type="date" format="DD.MM.YYYY"></date-picker>
+          </b-col>
+          <b-col cols="12" sm="6" md="12" lg="6" class="mt-3">
+            <date-picker class="w-100" v-model="endDate" lang="ru" type="date" format="DD.MM.YYYY"></date-picker>
+          </b-col>
           <br />
-          <b-form-group>
-            <h5 v-if="options.length === 0">Нет событий.</h5>
-            <b-form-checkbox-group
-              stacked
-              v-model="selected"
-              :options="options"
-            >
-            </b-form-checkbox-group>
-          </b-form-group>
-        </b-tab>
-        <b-tab title="За период" @click="setMode(false)">
-          <br />
-          <b-form-group label="Выберите период:">
-            <b-row>
-              <b-col cols="12" sm="6" md="12" lg="6" class="mt-3">
-                <date-picker class="w-100" v-model="startDate" lang="ru" type="date" format="DD.MM.YYYY"></date-picker>
-              </b-col>
-              <b-col cols="12" sm="6" md="12" lg="6" class="mt-3">
-                <date-picker class="w-100" v-model="endDate" lang="ru" type="date" format="DD.MM.YYYY"></date-picker>
-              </b-col>
-              <br />
-            </b-row>
-          </b-form-group>
-          <br />
-          <b-form-group label="Выберите тип:">
-            <h5 v-if="options.length === 0">Нет событий.</h5>
-            <b-form-checkbox-group
-              stacked
-              v-model="selected"
-              :options="options"
-            >
-            </b-form-checkbox-group>
-          </b-form-group>
-        </b-tab>
-      </b-tabs>
+        </b-row>
+      </b-form-group>
+      
+      <b-form-group label="Выберите тип:">
+        <h5 v-if="options.length === 0">Нет событий.</h5>
+        <b-form-checkbox-group
+          stacked
+          v-model="selected"
+          :options="options"
+        >
+        </b-form-checkbox-group>
+      </b-form-group>
       
       <template slot="modal-footer">
         <button
@@ -54,7 +37,7 @@
         <button
           type="button"
           class="btn btn-primary"
-          :disabled="selected.length == 0 && byType || isModalInProcess"
+          :disabled="isModalInProcess"
           @click="onSubmitModal"
         >Скачать</button>
       </template>
@@ -96,8 +79,6 @@ export default class CSummaryModal extends Vue {
   public startDate: Date = new Date();
   public endDate: Date = new Date();
 
-  public byType: boolean = true;
-
   private visibilityStuff: boolean = false;
 
   // Component methods //
@@ -124,103 +105,51 @@ export default class CSummaryModal extends Vue {
   // Methods //
   ////////////
 
-  public setMode(byType: boolean) {
-    if (byType === true) {
-      this.endDate = new Date();
-      this.startDate = new Date(new Date().setUTCFullYear(new Date().getUTCFullYear() - 1));
-    } else {
-      this.selected = [];
-    }
-
-    this.byType = byType;
-  }
-
   public async onSubmitModal() {
     this.isModalInProcess = true;
 
     let data: any = null;
 
-    if (this.byType) {
-      return new Promise((resolve, reject) => {
-        axios
-          .post('summary',
-            {
-              targetEventTypes: this.selected
-            },
-            {
-              responseType: 'blob'
-            }
-          )
-          .then((response) => {
-            if (response && response.status === 200) {
-            data = response.data;
-            const url = window.URL.createObjectURL(new Blob([data]));
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', 'summary.xlsx');
-            document.body.appendChild(link);
-            link.click();
-            this.isModalInProcess = false;
-            this.isModalVisible = false;
-            resolve();
-            } else {
-              this.$notify({
-                title: 'Проблемы с сервером, попробуйте позже.',
-                duration: 1500,
-                type: 'error'
-              });
-              this.isModalInProcess = false;
-              this.isModalVisible = false;
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-            this.isModalInProcess = false;
-            reject(error);
-          });
-      });
-    } else {
-      return new Promise((resolve, reject) => {
-        let types = '';
-        for (const S of this.selected) {
-          types += `&eventTypeId=${S}`;
-        }
+    return new Promise((resolve, reject) => {
+      let types = '';
+      for (const S of this.selected) {
+        types += `&eventTypeId=${S}`;
+      }
 
-        axios
-          .get(`docsgen/downloadxls?begin=${this.startDate.toISOString().slice(0, 10)}&end=${this.endDate.toISOString().slice(0, 10)}${types}`,
-            {
-              responseType: 'blob'
-            }
-          )
-          .then((response) => {
-            if (response && response.status === 200) {
-            data = response.data;
-            const url = window.URL.createObjectURL(new Blob([data]));
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', 'svodka.xlsx');
-            document.body.appendChild(link);
-            link.click();
+      axios
+        .get(`docsgen/downloadxls?begin=${this.startDate.toISOString().slice(0, 10)}&end=${this.endDate.toISOString().slice(0, 10)}${types}`,
+          {
+            responseType: 'blob'
+          }
+        )
+        .then((response) => {
+          if (response && response.status === 200) {
+          data = response.data;
+          const url = window.URL.createObjectURL(new Blob([data]));
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute('download', 'svodka.xlsx');
+          document.body.appendChild(link);
+          link.click();
+          this.isModalInProcess = false;
+          this.isModalVisible = false;
+          resolve();
+          } else {
+            this.$notify({
+              title: 'Проблемы с сервером, попробуйте позже.',
+              duration: 1500,
+              type: 'error'
+            });
             this.isModalInProcess = false;
             this.isModalVisible = false;
-            resolve();
-            } else {
-              this.$notify({
-                title: 'Проблемы с сервером, попробуйте позже.',
-                duration: 1500,
-                type: 'error'
-              });
-              this.isModalInProcess = false;
-              this.isModalVisible = false;
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-            this.isModalInProcess = false;
-            reject(error);
-          });
-      });
-    }
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isModalInProcess = false;
+          reject(error);
+        });
+    });
   }
 
   set isModalVisible(value: boolean) {
